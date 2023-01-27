0b5d9841ce278b7c2897df07dabdef25a98e62e8
Chayapat <papae_peez02@hotmail.co.th>
2023-01-27 16:59:51 +0700

    save

--- menu_left.php
+++ menu_left.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?php echo PATH; ?>" class="brand-link">
-        <img src="<?php echo PATH; ?>/img/logo1.png" class="brand-image img-circle elevation-3"
+        <img src="<?php echo PATH; ?>/img/logo_fb.png" class="brand-image img-circle elevation-3"
             style="background-color:white;">
         <span class="brand-text font-weight-light">Demo</span>
     </a>
@@ -12,7 +12,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?php echo PATH; ?>/AdminLTE-3.2.0/dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+                <img src="<?php echo PATH; ?>/img/default.jpg" class="img-circle elevation-2"
                     alt="User Image">
             </div>
             <div class="info">
